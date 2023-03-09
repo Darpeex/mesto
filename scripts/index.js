938c472a18229753be963d9b87ec88b3da69bdde
darpeex <foxyyysp@mail.ru>
2023-03-09 16:35:41 +0300

    Initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -3,26 +3,33 @@ const popupElement = document.querySelector('.popup');
 const popupEditButton = document.querySelector('.profile__info_edit-button');
 const popupCloseButton = popupElement.querySelector('.popup__submit-button_action_close');
 const popupSaveButton = popupElement.querySelector('.popup__submit-button_action_save');
+const getName = document.querySelector ('.profile__info-text_name');
+const getJob = document.querySelector('.profile__info-text_activity');
+const formElement = document.querySelector('.popup__container');
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('.popup__form-name');
+let jobInput = formElement.querySelector('.popup__form-activity');
 
-// Открываем попап
+// Открыть попап
 const openPopup = function () {
   popupElement.classList.add('popup_opened');
+  nameInput.value = getName.textContent;
+  jobInput.value = getJob.textContent;
 };
-popupEditButton.addEventListener('click', openPopup);
 
-// Закрываем попап
+// Закрыть попап
 const closePopup = function () {
   popupElement.classList.remove('popup_opened');
 };
+
+// 
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    getName.textContent = `${nameInput.value}`;
+    getJob.textContent = `${jobInput.value}`;
+    closePopup ();
+}
+
+popupEditButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
-
-
-
-
-
-// При открытии формы поля «Имя» и «О себе» должны быть заполнены теми значениями, которые отображаются на странице
-// Если пользователь закрывает попап нажав на крестик, то введённые значения не сохраняются. О том, как работает кнопка «Сохранить», расскажем дальше.
-//После внесения изменений и нажатия кнопки «Сохранить» информация на странице должна обновиться, а попап автоматически закрыться:
-// Специальное событие submit отправляет форму (его мы ещё не проходили). Перед вами шаблон кода, реализующий его обработку. Постарайтесь в нём разобраться. Мы оставили в коде комментарии, которые с этим помогут:
-
-//Посмотреть на Я.П
+formElement.addEventListener('submit', handleFormSubmit);
