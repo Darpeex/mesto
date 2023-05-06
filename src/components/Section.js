e985092f4050b6740233a2d0aa463afa7ba84c32
darpeex <foxyyysp@mail.ru>
2023-05-06 15:27:44 +0300

    const в utils.js + class Section.js

---
+++ src/components/Section.js
@@ -0,0 +1,21 @@
+class Section {
+  // Свойство items — это массив данных, которые нужно добавить на страницу при инициализации класса. 
+  // Свойство renderer — это функция, которая отвечает за создание и отрисовку данных на странице.
+  // Второй параметр конструктора — селектор контейнера, в который нужно добавлять созданные элементы.
+  constructor({ items, renderer }, selector) {
+    this.#renderedItems = items;
+    this.#renderer = renderer;
+    
+    this.#container = document.querySelector(selector);
+  }
+
+  // Метод, который отвечает за отрисовку всех элементов. 
+  renderItems() {
+    this.#renderedItems.forEach(item => this.#renderer(item))
+  }
+
+  // Метод, который принимает DOM-элемент и добавляет его в контейнер.
+  addItem(element) {
+    this.#container.append(element);
+  }
+}
