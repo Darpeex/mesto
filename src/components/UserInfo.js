52922263bb6161aa1ea905398d7c4a50eb237dbc
darpeex <foxyyysp@mail.ru>
2023-05-07 19:21:53 +0300

    Class UserInfo

---
+++ src/components/UserInfo.js
@@ -0,0 +1,21 @@
+class UserInfo {
+  #profileNameSelector;
+  #profileAboutSelector;
+  // Новые комментарии после сдачи спринта пишу
+  constructor({ profileNameSelector, profileAboutSelector }) {
+    this.#profileNameElement = document.querySelector(profileNameSelector);
+    this.#profileDescriptionElement = document.querySelector(profileAboutSelector);
+  }
+
+  getUserInfo() {
+    return {
+      userName: this.#profileNameElement.textContent,
+      userDescription: this.#profileDescriptionElement.textContent
+    }
+  }
+
+  setUserInfo({ userName, userDescription }) {
+    this.#profileNameElement.textContent = userName;
+    this.#profileDescriptionElement.textContent = userDescription;
+  }
+}
